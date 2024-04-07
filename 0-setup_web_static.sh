6ba5683cef8642c03ae9a8820bfcb96a6ef245d8
MahoneyT1 <ifeanyitech15@gmail.com>
2024-04-07 11:33:50 -0700

    update

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,26 +1,17 @@
 #!/usr/bin/env bash
-# a Bash script that sets up your web servers for the deployment of web_static
-
-# Install Nginx if not already installed
+# Bash scrip to set up web servers to deploy web_static
 sudo apt-get update
 sudo apt-get -y install nginx
-
-# Create necessary folders
-sudo mkdir -p /data/web_static/releases/test /data/web_static/shared
-
-# Create fake HTML file
-echo "<html><head></head><body>Holberton School</body></html>" | sudo tee /data/web_static/releases/test/index.html
-
-# Create symbolic link
+sudo mkdir -p /data/web_static/shared/
+sudo mkdir -p /data/web_static/releases/test/
+sudo echo "<html>
+  <head>
+  </head>
+  <body>
+    Holberton School
+  </body>
+</html>" | sudo tee /data/web_static/releases/test/index.html
 sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
-
-# Set ownership
-sudo chown -R ubuntu:ubuntu /data/
-
-# Update Nginx configuration
-sudo sed -i 's|^.*server_name.*$|&\n\n\tlocation /hbnb_static {\n\t\talias /data/web_static/current/;\n\t}\n|' /etc/nginx/sites-available/default
-
-# Restart Nginx
+sudo chown -R ubuntu:ubuntu /data
+sudo sed -i '53i \\tlocation \/hbnb_static {\n\t\t alias /data/web_static/current;\n\t}' /etc/nginx/sites-available/default
 sudo service nginx restart
-
-exit 0
